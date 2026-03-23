48755d21a29be9caa16490f8f3faa29d7b8417bd
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-03-23 13:19:43 +0100

    feat: Interswitch payment initiation endpoint working

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const paymentRoutes = require('./routes/payment');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/payment', paymentRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'MediRemit API running' });
